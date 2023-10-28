2e1a7bce601d782ed858940717451799730b9df1
rivaalms <rvalms@icloud.com>
2023-10-28 16:39:29 +0700

    build: app configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+   devtools: {
+      enabled: true
+   },
+
+   css: [
+      '~/assets/css/base.css'
+   ],
+
+   modules: [
+      '@pinia/nuxt',
+      '@pinia-plugin-persistedstate/nuxt',
+      '@nuxt/ui',
+      'dayjs-nuxt'
+   ],
+
+   postcss: {
+      plugins: {
+         tailwindcss: {},
+         autoprefixer: {},
+      },
+   },
+
+   ui: {
+      safelistColors: [
+         'blue',
+         'gray',
+         'zinc',
+         'red',
+         'amber',
+         'green',
+         'cyan',
+      ],
+   },
+
+   colorMode: {
+      preference: 'light'
+   },
+
+   dayjs: {
+      locales: [ 'id', 'en' ],
+      defaultLocale: 'id',
+      defaultTimezone: 'Asia/Jakarta'
+   }
 })
