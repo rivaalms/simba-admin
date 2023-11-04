7ccc6599fbacb4a8101d286009b0d35443a34466
rivaalms <rvalms@icloud.com>
2023-11-04 14:08:39 +0700

    build: configure postcss nesting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,6 +49,8 @@ export default defineNuxtConfig({
 
    postcss: {
       plugins: {
+         'postcss-import': {},
+         'tailwindcss/nesting': {},
          tailwindcss: {},
          autoprefixer: {},
       },
