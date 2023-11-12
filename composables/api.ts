daa707296efd796fc832618a8c7f0927d077b7e6
rivaalms <rvalms@icloud.com>
2023-11-12 10:31:58 +0700

    perf: alter useAPI
    use useFetch instead of $fetch

--- composables/api.ts
+++ composables/api.ts
@@ -1,29 +1,34 @@
-export const useAPI = async (url: string, opts?: {}) => await $fetch (url, {
-   baseURL: useRuntimeConfig().public.apiBaseUrl,
-   ...opts,
-   async onRequest({ options, error }) {
-      const headers : { [key: string]: any } = {
-         ...options.headers,
-         Accept: 'application/json',
-         UserableType: null,
+import type { UseFetchOptions } from "nuxt/app"
+
+export const useAPI = async (url: string, opts?: UseFetchOptions<any>) : Promise <unknown> => {
+   const { data } = await useFetch(url, {
+      baseURL: useRuntimeConfig().public.apiBaseUrl,
+      ...opts,
+      async onRequest({ options, error }) {
+         const headers : { [key: string]: any } = {
+            ...options.headers,
+            Accept: 'application/json',
+            UserableType: null,
+         }
+         if (useAuthStore().isLoggedIn) {
+            headers.Authorization = `Bearer ${useAuthStore().getToken}`
+         }
+         options.headers = headers
+         if (error) throw error
+      },
+      async onRequestError({ error }) { throw error },
+      async onResponse({ response, error }) {
+         if (error) throw error
+         return response._data
+      },
+      async onResponseError({ response }) {
+         if (response.status === 401) {
+            useAuthStore().$reset()
+            if (localStorage.getItem('user')) localStorage.removeItem('user')
+            if (localStorage.getItem('token')) localStorage.removeItem('token')
+            navigateTo('/login')
+         }
       }
-      if (useAuthStore().isLoggedIn) {
-         headers.Authorization = `Bearer ${useAuthStore().getToken}`
-      }
-      options.headers = headers
-      if (error) throw error
-   },
-   async onRequestError({ error }) { throw error },
-   async onResponse({ response, error }) {
-      if (error) throw error
-      return response._data
-   },
-   async onResponseError({ response }) {
-      if (response.status === 401) {
-         useAuthStore().$reset()
-         if (localStorage.getItem('user')) localStorage.removeItem('user')
-         if (localStorage.getItem('token')) localStorage.removeItem('token')
-         navigateTo('/login')
-      }
-   }
-})
+   })
+   return data.value
+}
