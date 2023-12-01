ac4f49770fddf6e90841d44d54e6e5ce79a249b8
rivaalms <rvalms@icloud.com>
2023-12-01 21:22:58 +0700

    refactor: API request error handling

--- utils/apis/index.ts
+++ utils/apis/index.ts
@@ -4,28 +4,42 @@ export const $api = async (url: string, opts?: UseFetchOptions<any>) : Promise <
    const { data } = await useFetch(url, {
       baseURL: useRuntimeConfig().public.apiBaseUrl,
       ...opts,
-      async onRequest({ options, error }) {
+
+      async onRequest({ options }) {
          const headers : { [key: string]: any } = {
             ...options.headers,
             Accept: 'application/json',
-            UserableType: null,
          }
          if (useAuthStore().isLoggedIn) {
             headers.Authorization = `Bearer ${useAuthStore().getToken}`
          }
          options.headers = headers
-         if (error) throw error
       },
-      async onRequestError({ error }) { throw error },
-      async onResponse({ response, error }) {
-         if (error) throw error
+
+      async onRequestError({ error }) {
+         useAppStore().notify('error', `${error.message}`, 'api-request-error')
+      },
+
+      async onResponse({ response }) {
          return response._data
       },
+
       async onResponseError({ response }) {
-         if (response.status === 401) {
+         const { status, _data } = response
+
+         if (status === 403) {
+            useAppStore().notify('error', 'Akses tidak diizinkan', 'api-response-error')
+            return
+         }
+
+         const message = `${status}: ${_data?.message}`
+
+         useAppStore().notify('error', message, 'api-response-error')
+
+         if (status === 401) {
             useAuthStore().$reset()
-            if (localStorage.getItem('user')) localStorage.removeItem('user')
-            if (localStorage.getItem('token')) localStorage.removeItem('token')
+            localStorage.removeItem('user')
+            localStorage.removeItem('token')
             navigateTo('/login')
          }
       }
