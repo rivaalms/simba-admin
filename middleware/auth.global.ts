6f92da033b1d2712b1cfa88163bd00bd370f6434
Riva Almero <riva@dev.netkromsolution.com>
2023-12-01 12:46:38 +0700

    fix: user cache does not cleared when session expired

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,12 @@
 export default defineNuxtRouteMiddleware((to, from) => {
    const authStore = useAuthStore()
 
-   if (to.name !== 'login' && !authStore.isLoggedIn) return navigateTo('/login')
+   if (to.name !== 'login' && !authStore.isLoggedIn)
+   {
+      authStore.$reset()
+      if (localStorage.getItem('user')) localStorage.removeItem('user')
+      if (localStorage.getItem('token')) localStorage.removeItem('token')
+      return navigateTo('/login')
+   }
    else if (to.name === 'login' && authStore.isLoggedIn) return navigateTo('/')
 })
