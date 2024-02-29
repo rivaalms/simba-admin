b44fe7fdd25ed7251bac6a167525b2ff9fea2492
rivaalms <rvalms@icloud.com>
2024-02-29 10:34:38 +0700

    refactor: wrap mapper with promise

--- utils/map-teachers.ts
+++ utils/map-teachers.ts
@@ -5,43 +5,47 @@ type Map = {
 }
 
 export const mapTeachers = async (teachers: Model.School.Teacher[], subjects: Util.Subject[]) : Promise <Util.MapTeacher[]> => {
-   // Initialize an empty array to store the map data
-   const mapData : Map[] = []
+   return await new Promise(resolve => {
+      // Initialize an empty array to store the map data
+      const mapData : Map[] = []
 
-   teachers.forEach(item => {
-      // Get the timestamp of the teacher's last update
-      const updatedAt = new Date(item.updated_at!).getTime()
+      teachers.forEach(item => {
+         // Get the timestamp of the teacher's last update
+         const updatedAt = new Date(item.updated_at!).getTime()
 
-      // Find existing data in mapData with the same subject
-      const existingData = mapData.find(data => data.subject === item.subject!.name)
+         // Find existing data in mapData with the same subject
+         const existingData = mapData.find(data => data.subject === item.subject!.name)
 
-      // If no existing data found or the teacher's update is more recent, push the current iteration data to mapData
-      if (!existingData || updatedAt > existingData.updated!) {
-         mapData.push({
-            subject: item.subject?.name!,
-            value: item.count,
-            updated: updatedAt
-         })
-      }
+         // If no existing data found or the teacher's update is more recent, push the current iteration data to mapData
+         if (!existingData || updatedAt > existingData.updated!) {
+            mapData.push({
+               subject: item.subject?.name!,
+               value: item.count,
+               updated: updatedAt
+            })
+         }
+      })
+
+      // Iterate through each subject (for including all subjects that has no teacher)
+      subjects.forEach(subjectItem => {
+         // Find existing data in mapData with the same subject
+         const existingData = mapData.find(data => data.subject === subjectItem.name)
+
+         // If no existing data found, push the current iteration data to mapData with a value of 0
+         if (!existingData) {
+            mapData.push({
+               subject: subjectItem.name,
+               value: 0,
+               updated: null
+            })
+         }
+      })
+
+      // Remove the 'updated' property from the mapData, then sort the data in descending order based on 'value'
+      resolve(
+         mapData
+         .map(({ updated, ...rest }) => rest)
+         .sort((a, b) => b.value - a.value)
+      )
    })
-
-   // Iterate through each subject (for including all subjects that has no teacher)
-   subjects.forEach(subjectItem => {
-      // Find existing data in mapData with the same subject
-      const existingData = mapData.find(data => data.subject === subjectItem.name)
-
-      // If no existing data found, push the current iteration data to mapData with a value of 0
-      if (!existingData) {
-         mapData.push({
-            subject: subjectItem.name,
-            value: 0,
-            updated: null
-         })
-      }
-   })
-
-   // Remove the 'updated' property from the mapData, then sort the data in descending order based on 'value'
-   return mapData
-      .map(({ updated, ...rest }) => rest)
-      .sort((a, b) => b.value - a.value)
 }
